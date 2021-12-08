3bc885193174abb6c5297ead0163ce3350d78dd6
ih <ih-cs6300>
2021-12-08 03:13:34 -0500

    for-loop in run_df.sh

--- run_df.sh
+++ run_df.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-rm ./pred_result2/*
-python3 test1.py
-rm core.*
+for (( idx=1; idx<=$1; idx++ ))
+do
+   echo "Bash loop $idx"
+   rm ./pred_result2/*
+   python3 test1.py
+   rm core.*
+   echo ""
+done
