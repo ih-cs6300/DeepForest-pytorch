5b92333f19451c566542fde410bc40d41d11286c
ih <ih-cs6300>
2021-12-27 18:19:29 -0500

    sjer settings

--- test1.py
+++ test1.py
@@ -19,17 +19,17 @@ np.random.seed(42)
 n_classes = 1
 rules = [FOL_bbox_2big(device, 1, None, None), ]   #[FOL_green(device, 2, None, None), ]
 rule_lambdas = [1e2]
-pi_params = [0.95, 0.5]  #0.9, 0
+pi_params = [0.8, 0.5]  
 batch_size = 1
-C = 0.01  # 6
+C = 0.000125  # 6
 
 # directory with image and annotation data
-train_dir = "/blue/daisyw/iharmon1/data/DeepForest-pytorch/training3"
-eval_dir = "/blue/daisyw/iharmon1/data/DeepForest-pytorch/evaluation3"
+train_dir = "/blue/daisyw/iharmon1/data/DeepForest-pytorch/training3_bak"
+eval_dir = "/blue/daisyw/iharmon1/data/DeepForest-pytorch/evaluation3_bak"
 
-train_csv = os.path.join(train_dir, "TEAK-train.csv")
-val_csv = os.path.join(train_dir, "TEAK-val.csv")
-test_csv = os.path.join(eval_dir, "TEAK-test.csv")
+train_csv = os.path.join(train_dir, "SJER-train.csv")
+val_csv = os.path.join(train_dir, "SJER-val.csv")
+test_csv = os.path.join(eval_dir, "SJER-test.csv")
 
 """## Training & Evaluating Using GPU"""
 
@@ -50,7 +50,7 @@ m.config["train"]["lr"] = 0.0017997179587414414  # default 0.001
 print("Training csv: {}".format(m.config["train"]["csv_file"]))
 
 training_data = m.train_dataloader()
-m.config["train"]["beg_incr_pi"] = round(len(training_data) * 4)
+m.config["train"]["beg_incr_pi"] = round(len(training_data) * 3)
 
 n_train_batches = len(training_data) / batch_size
 m.config["train"]["n_train_batches"] = n_train_batches
@@ -83,7 +83,11 @@ for f in file_list[:34]:
    comet.experiment.log_image('./pred_result2/' + f)
 
 comet.experiment.add_tags([os.path.basename(test_csv).split('-')[0].lower()])
-comet.experiment.log_others(results)
+comet.experiment.log_others({'box_precision': results['box_precision'], 
+			     'box_recall': results['box_recall'], 
+                             'class_precision': results['class_recall']['precision'].item(), 
+                             'class_recall': results['class_recall']['recall'].item(), 
+                             'size': results['class_recall']['size'].item()})
 comet.experiment.log_parameter('pi_params', pi_params)
 comet.experiment.log_parameter('m.config', m.config)
 comet.experiment.log_parameter("m.config['train']", m.config['train'])
