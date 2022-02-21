483d396606038d65a919464f9e42ab64e467a431
ih <ih-cs6300>
2022-02-21 12:27:44 -0500

    added logging and seed

--- run_df.sh
+++ run_df.sh
@@ -4,7 +4,8 @@ for (( idx=1; idx<=$1; idx++ ))
 do
    echo "Bash loop $idx"
    rm ./pred_result2/*
-   python3 test1.py
+   seed=`awk "FNR == $idx" ./seeds.txt`
+   python3 test1.py --seed $seed
    rm core.*
    echo ""
 done
--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 # load modules
 import comet
+import argparse
 import os
 import time
 import numpy as np
@@ -15,33 +16,40 @@ from deepforest.fol import FOL_green, FOL_competition, FOL_bbox_2big
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-np.random.seed(42)
 n_classes = 1
 rules = [FOL_bbox_2big(device, 1, None, None), ]   #[FOL_green(device, 2, None, None), ]
 rule_lambdas = [1e2]
-pi_params = [0.8, 0.5]  
+pi_params = [0.95, 0.5]  
 batch_size = 1
 C = 0.000125  # 6
 
-# directory with image and annotation data
-train_dir = "/blue/daisyw/iharmon1/data/DeepForest-pytorch/training3_bak"
-eval_dir = "/blue/daisyw/iharmon1/data/DeepForest-pytorch/evaluation3_bak"
+# parser stuff
+parser = argparse.ArgumentParser()
+parser.add_argument("--seed", type=int, default=42)
+args = parser.parse_args()
 
-train_csv = os.path.join(train_dir, "SJER-train.csv")
-val_csv = os.path.join(train_dir, "SJER-val.csv")
-test_csv = os.path.join(eval_dir, "SJER-test.csv")
+# directory with image and annotation data
+train_dir = "/blue/daisyw/iharmon1/data/DeepForest-pytorch/training3"
+eval_dir = "/blue/daisyw/iharmon1/data/DeepForest-pytorch/evaluation3"
+
+train_csv = os.path.join(train_dir, "NIWO-train.csv")
+val_csv = os.path.join(train_dir, "NIWO-val.csv")
+test_csv = os.path.join(eval_dir, "NIWO-test.csv")
 
 """## Training & Evaluating Using GPU"""
 
 # initialize the model and change the corresponding config file
-torch.manual_seed(42)
-torch.cuda.manual_seed_all(42)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+np.random.seed(args.seed)
+
+
 m = main.deepforest(rules, rule_lambdas, pi_params, C, num_classes=n_classes).to(device)
 m.config['gpus'] = '-1' #move to GPU and use all the GPU resources
 m.config["train"]["csv_file"] = train_csv
 m.config["train"]["root_dir"] = train_dir
 m.config["score_thresh"] = 0.46  # default 0.4
-m.config["train"]['epochs'] = 5
+m.config["train"]['epochs'] = 7
 m.config["validation"]["csv_file"] = val_csv
 m.config["validation"]["root_dir"] = train_dir
 m.config["nms_thresh"] = 0.57  # default 0.05
@@ -50,7 +58,7 @@ m.config["train"]["lr"] = 0.0017997179587414414  # default 0.001
 print("Training csv: {}".format(m.config["train"]["csv_file"]))
 
 training_data = m.train_dataloader()
-m.config["train"]["beg_incr_pi"] = round(len(training_data) * 3)
+m.config["train"]["beg_incr_pi"] = round(len(training_data) * 5)
 
 n_train_batches = len(training_data) / batch_size
 m.config["train"]["n_train_batches"] = n_train_batches
@@ -76,6 +84,24 @@ except OSError as error:
    pass
 
 results = m.evaluate(test_csv, eval_dir, iou_threshold = 0.5, show_plot = False, savedir= save_dir)
+print("bbox prec: {}".format(results['box_precision']))
+print("bbox rec: {}".format(results['box_recall']))
+print("class prec: {}".format(results['class_recall']['precision'].item()))
+print("class rec: {}".format(results['class_recall']['recall'].item()))
+
+#######################################################################################################################################################################################################
+# log data to locally
+log_fname = "df_niwo_junk_log.csv"
+if not (os.path.isfile(log_fname)):
+   f = open(log_fname, "w")
+   f.write("train,test,epochs,seed,bbox_prec,bbox_rec,class_prec,class_rec\n")
+   f.close()
+
+f = open(log_fname, "a")
+f.write("{},{},{},{},{},{},{}\n".format(train_csv, test_csv, m.config["train"]['epochs'], args.seed, results['box_precision'], results['box_recall'], results['class_recall']['precision'].item(), 
+          results['class_recall']['recall'].item()))
+f.close()
+######################################################################################################################################################################################################
 
 file_list = [f for f in os.listdir(save_dir) if (f.split(".")[1] == 'png') or (f.split(".")[1] =='tif')]
 
